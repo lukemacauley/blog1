649fd974698416ed696ab37d43ecabce3acff04b
Luke Macauley <luke@lukemacauley.com>
2013-10-08 01:53:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog
+
+Simple blog platform with markdown interface.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog1::Application.config.secret_key_base = 'c6859a0da55663eeb2947297ebc8b520e63de458afa9b8883ca04dd272356038f0329dc386d92e0dc32608871f84ad7d2677d586f917b08e6f2c236c1c91633c'
