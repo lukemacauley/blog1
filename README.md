61d320c4b7ae0a963d4c2d59930d2e6c384e76d7
Luke Macauley <luke@lukemacauley.com>
2013-10-11 13:57:28 +0100

    Update Devise routes

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Simple blog platform with markdown interface.
 - Add CanCan to prevent anyone viewing or editings Drafts
 - Add Rolify for Admin role
 - Learn how to create settings
-- Change Devise's routes
+- ~~Change Devise's routes~~
+- Change User Log In to username and password, change User Sign Up to full name, username, email, password
 - Add styling to whole blog platform
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Blog1::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, path: "", path_names: { sign_in: 'login', sign_out: 'logout', confirmation: 'verification', unlock: 'unblock', registration: 'account', sign_up: 'new' }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
